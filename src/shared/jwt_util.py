ed8a96b65f317bcc58b9e78e9276a43751e62c76
wuriyanto <wuriyanto48@yahoo.co.id>
2018-10-21 20:36:02 +0700

    add jwt util

---
+++ src/shared/jwt_util.py
@@ -0,0 +1,31 @@
+import jwt
+
+from src.server.server import app
+from datetime import datetime, timedelta
+
+class JwtUtil:
+
+    @staticmethod
+    def encode(sub):
+        """ Generate access token """
+        try:
+            payload = {
+                'exp': datetime.utcnow() + timedelta(days=0, seconds=5),
+                'iat': datetime.utcnow(),
+                'iss': 'purwokertodev.github.io',
+                'sub': sub
+            }
+
+            return jwt.encode(payload, app.config.get('SECRET_KEY'), algorithm='HS256')
+        except Exception as e:
+            return e
+
+    @staticmethod
+    def decode(access_token):
+        try:
+            payload = jwt.decode(access_token, app.config.get('SECRET_KEY'))
+            return payload['sub']
+        except jwt.ExpiredSignatureError:
+            return 'access token expired'
+        except jwt.InvalidTokenError:
+            return 'access token invalid'
