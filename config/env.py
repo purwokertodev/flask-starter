d337c6efe854996a3f6700231d7b48f0aa3230db
wuriyanto <wuriyanto48@yahoo.co.id>
2018-10-21 20:34:23 +0700

    update config

--- config/env.py
+++ config/env.py
@@ -7,7 +7,7 @@ database_name = 'flask_auth'
 
 class BaseConfig:
     """Base configuration."""
-    SECRET_KEY = os.getenv('SECRET_KEY', 'my_precious')
+    SECRET_KEY = os.getenv('SECRET_KEY', '123456')
     DEBUG = False
     BCRYPT_LOG_ROUNDS = 13
     SQLALCHEMY_TRACK_MODIFICATIONS = False
