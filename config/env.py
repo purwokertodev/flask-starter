07519c480dde4b364d1a1b3f127890ea73a8fc9c
wuriyanto <wuriyanto48@yahoo.co.id>
2018-10-21 18:28:46 +0700

    add development config

---
+++ config/env.py
@@ -0,0 +1,20 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+postgres_local_base = 'postgresql://wurianto:123456@localhost/'
+database_name = 'flask_auth'
+
+
+class BaseConfig:
+    """Base configuration."""
+    SECRET_KEY = os.getenv('SECRET_KEY', 'my_precious')
+    DEBUG = False
+    BCRYPT_LOG_ROUNDS = 13
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+class DevelopmentConfig(BaseConfig):
+    """Development configuration."""
+    DEBUG = True
+    BCRYPT_LOG_ROUNDS = 4
+    SQLALCHEMY_DATABASE_URI = postgres_local_base + database_name
